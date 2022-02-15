200393fac651b040f5234bf0850213f593a85280
MadLord667 <96691151+MadLord667@users.noreply.github.com>
2022-02-15 18:52:33 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+const item = document.querySelector('.item')
+const placeholders = document.querySelectorAll('.placeholder')
+
+item.addEventListener('dragstart', dragstart)
+item.addEventListener('dragend', dragend)
+
+for (const placeholder of placeholders) {
+    placeholder.addEventListener('dragover', dragover)
+    placeholder.addEventListener('dragenter', dragenter)
+    placeholder.addEventListener('dragleave', dragleave)
+    placeholder.addEventListener('drop', dragdrop)
+
+
+
+}
+
+function dragstart(event) {
+    event.target.classList.add('hold')
+    setTimeout(() => event.target.classList.add('hide'), 0)
+}
+
+function dragend(event) {
+    // 1-й вариант:
+    // event.target.classList.remove('hold')
+    // event.target.classList.remove('hide')
+
+    // 2-й вариант:
+    // event.target.classList.remove('hold', 'hide')
+
+    // 3-й вариант:
+    event.target.className = 'item'
+}
+
+function dragover(event) {
+    event.preventDefault()
+}
+
+function dragenter(event) {
+    event.target.classList.add('hovered')
+}
+
+function dragleave(event) {
+    event.target.classList.remove('hovered')
+}
+
+function dragdrop(event) {
+    event.target.classList.remove('hovered')
+    event.target.append(item)
+}
